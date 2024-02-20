54e9d17b1873b134086ed309394a71273796e59a
moonemoji <amsy12345@gmail.com>
2024-02-20 14:20:06 -0600

    removed dynamic changelog

--- scripts/game_structure/windows.py
+++ scripts/game_structure/windows.py
@@ -931,22 +931,22 @@ class ChangelogPopup(UIWindow):
         if get_version_info().is_dev() and not get_version_info().is_source_build:
             dynamic_changelog = True
 
-        if dynamic_changelog:
-            commits = file_cont.splitlines()
-            file_cont = ""
-            for line in commits:
-                info = line.split("|||")
+        # if dynamic_changelog:
+        #     commits = file_cont.splitlines()
+        #     file_cont = ""
+        #     for line in commits:
+        #         info = line.split("|||")
                                 
-                # Get PR number so we can link the PR
-                # pr_number = re_search(r"Merge pull request #([0-9]*?) ", info[3])
-                # if pr_number:
+        #         # Get PR number so we can link the PR
+        #         pr_number = re_search(r"Merge pull request #([0-9]*?) ", info[3])
+        #         if pr_number:
                     
-                #     # For some reason, multi-line links on pygame_gui's text boxes don't work very well. 
-                #     # So, to work around that, just add a little "link" at the end
-                #     info[2] += f" <a href='https://github.com/ClanGenOfficial/clangen/pull/{pr_number.group(1)}'>(link)</a>"
+        #             # For some reason, multi-line links on pygame_gui's text boxes don't work very well. 
+        #             # So, to work around that, just add a little "link" at the end
+        #             info[2] += f" <a href='https://github.com/ClanGenOfficial/clangen/pull/{pr_number.group(1)}'>(link)</a>"
                 
-                # Format: DATE- \n PR Title (link)
-                file_cont += f"<b>{info[1]}</b>\n- {info[2]}\n"
+        #         # Format: DATE- \n PR Title (link)
+        #         file_cont += f"<b>{info[1]}</b>\n- {info[2]}\n"
 
         self.changelog_text = UITextBoxTweaked(
             file_cont,
